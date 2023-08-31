896113e411cf7aa89763ae6bdef5b2a56648c2a8
Ilmari "ilzu" Siiteri <ilzu.siiteri@gmail.com>
2023-08-31 03:11:24 +0000

    Should have read the whole question before coding....

--- 6/6.6.py
+++ 6/6.6.py
@@ -7,10 +7,17 @@ def calculate(diameter, price):
     return price / area
 
 def main():
-    diameter = float(input("Anna pizzan halkaisija senttimetreinä: "))
-    price = float(input("Anna pizzan hinta euroina: "))
-    print(f"Pizzan hinta neliömetriltä on {calculate(diameter, price)}")
-
+    unitprice = [0, 0]
+    for i in range(2):
+        diameter = float(input(f"Anna pizzan {i + 1} halkaisija senttimetreinÃ¤: "))
+        price = float(input(f"Anna pizzan {i + 1} hinta euroina: "))
+        unitprice[i] = calculate(diameter, price)
+    if unitprice[0] > unitprice[1]:
+        print(f"Pizzan 2 yksikkÃ¶hinta on pienemp ({unitprice[0]} vs {unitprice[1]}).")
+    elif unitprice[0] < unitprice[1]:
+        print(f"Pizzan 1 yksikkÃ¶hinta on pienemp ({unitprice[0]} vs {unitprice[1]}).")
+    else:
+        print(f"Pizzojen yksikkÃ¶hinta on sam ({unitprice[0]}).")
 
 if __name__ == "__main__":
     main()
