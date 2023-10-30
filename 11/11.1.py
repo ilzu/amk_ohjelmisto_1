4fcf43f4a44b56805b5172f43659b447ad17f26e
Ilmari "ilzu" Siiteri <ilzu.siiteri@gmail.com>
2023-10-30 08:04:22 +0000

    muutettu 11.1

--- 11/11.1.py
+++ 11/11.1.py
@@ -4,8 +4,8 @@ class Julkaisu:
     def __init__(self, nimi):
         self.nimi = nimi
 
-    def tulosta_tiedot(self):
-        print(f"nimi: {self.nimi}")
+    def __str__(self):
+        return f"nimi: {self.nimi}"
 
 class Kirja(Julkaisu):
     def __init__(self, nimi, kirjoittaja, sivumaara):
@@ -13,25 +13,23 @@ class Kirja(Julkaisu):
         self.sivumaara = sivumaara
         super().__init__(nimi)
 
-    def tulosta_tiedot(self):
-        super().tulosta_tiedot()
-        print(f" kirjoittaja: {self.kirjoittaja} sivumäärä: {self.sivumaara}")
+    def __str__(self):
+        return super().__str__() + f" kirjoittaja: {self.kirjoittaja} sivumäärä: {self.sivumaara}"
 
 class Lehti(Julkaisu):
     def __init__(self, nimi, paatoimittaja):
         self.paatoimittaja = paatoimittaja
         super().__init__(nimi)
 
-    def tulosta_tiedot(self):
-        super().tulosta_tiedot()
-        print(f" päätoimittaja: {self.paatoimittaja}")
+    def __str__(self):
+         return super().__str__() + f" päätoimittaja: {self.paatoimittaja}"
 
 def main():
     kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
     lehti = Lehti("Aku Ankka", "Aki Hyyppä")
 
-    kirja.tulosta_tiedot()
-    lehti.tulosta_tiedot()
+    print(kirja)
+    print(lehti)
     
 if __name__ == "__main__":
     main()
